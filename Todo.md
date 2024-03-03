a575a579c1be8248c715b30f160316b5dcb8a382
Marvin Brouwer <marvin-brouwer@live.nl>
2024-03-03 15:36:55 +0100

    Added command name resolver

--- Todo.md
+++ Todo.md
@@ -19,3 +19,7 @@ We don't want to be releasing to npm manually, use one of those setups to make t
 - <https://www.hamzak.xyz/blog-posts/release-please-vs-semantic-release#:~:text=One%20key%20difference%20between%20release,into%20a%20project's%20build%20process>
 - Automatically publish and maintain npm packages:  
   <https://youtu.be/0Z0nAJjmQRg?si=W6IgYRqOe6I6sEW4>
+  
+## Test package with install global and dlx
+
+See if the name resolver works the way we think it does.  
--- libs/cli/src/index.ts
+++ libs/cli/src/index.ts
@@ -4,8 +4,24 @@ import { mapHttpArgument, registerHttp } from './commands/http';
 import { mapHttpsArgument, registerHttps } from './commands/https';
 import packageConfig from '../package.json' assert { type: 'json' };
 
+/**
+ * Since we support dlx, global install, and normal dependency install,
+ * we reflect back the command used in the help sceen.
+*/
+const getName = () => {
+	const commandArgs = process.argv.slice(0, 3);
+	// TODO: Remove once tested
+	// eslint-disable-next-line
+	console.log(commandArgs)
+	if (commandArgs.some((arg) => arg === 'npx')) return `npx ${packageConfig.name}`;
+	if (commandArgs.some((arg) => arg === 'pnpm') && commandArgs.some((arg) => arg === 'dlx')) return `pnpm dlx ${packageConfig.name}`;
+	if (commandArgs.some((arg) => arg === 'yarn') && commandArgs.some((arg) => arg === 'dlx')) return `yarn dlx ${packageConfig.name}`;
+
+	return 'lt';
+};
+
 program
-	.name('lt')
+	.name(getName())
 	.description(packageConfig.description)
 	.allowExcessArguments(false)
 	.allowUnknownOption(false)
